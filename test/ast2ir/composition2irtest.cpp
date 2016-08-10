27e419ee73fa9b5395a2a378e3e9d1c3b56a1d1b
starg <starg@users.osdn.me>
2016-08-10 23:42:12 +0900

    符号あり・符号なし整数の比較の警告に対処

--- test/ast2ir/composition2irtest.cpp
+++ test/ast2ir/composition2irtest.cpp
@@ -34,26 +34,26 @@ BOOST_AUTO_TEST_CASE(SimpleCommand1)
     auto ir = compiler.GetIR().value();
 
     BOOST_CHECK_EQUAL(ir.Name, "test.ym1");
-    BOOST_CHECK_EQUAL(ir.Blocks.size(), 0);
+    BOOST_CHECK_EQUAL(ir.Blocks.size(), 0u);
     BOOST_CHECK(ir.BlockNameMap.empty());
-    BOOST_CHECK_EQUAL(ir.TrackBlocks.size(), 1);
+    BOOST_CHECK_EQUAL(ir.TrackBlocks.size(), 1u);
 
     auto itTrack = ir.TrackBlockNameMap.find("MyComposition");
     BOOST_REQUIRE(itTrack != ir.TrackBlockNameMap.end());
 
     auto trackRef = itTrack->second;
-    BOOST_CHECK_EQUAL(trackRef.ID, 0);
+    BOOST_CHECK_EQUAL(trackRef.ID, 0u);
 
     auto trackBlock = ir.TrackBlocks.at(trackRef.ID);
-    BOOST_REQUIRE_EQUAL(trackBlock.Blocks.size(), 1);
+    BOOST_REQUIRE_EQUAL(trackBlock.Blocks.size(), 1u);
 
     auto command = boost::get<AST::Command>(trackBlock.Blocks.at(0));
-    BOOST_CHECK_EQUAL(command.Location.Line, 1);
-    BOOST_CHECK_EQUAL(command.Location.Column, 20);
+    BOOST_CHECK_EQUAL(command.Location.Line, 1u);
+    BOOST_CHECK_EQUAL(command.Location.Column, 20u);
 
     BOOST_CHECK_EQUAL(command.Name, "program");
     
-    BOOST_REQUIRE_EQUAL(command.Arguments.size(), 2);
+    BOOST_REQUIRE_EQUAL(command.Arguments.size(), 2u);
     BOOST_CHECK_EQUAL(boost::get<long>(command.Arguments.at(0).Value), 1);
     BOOST_CHECK_EQUAL(boost::get<long>(command.Arguments.at(1).Value), 16);
 }
--- test/parser/parsertest.cpp
+++ test/parser/parsertest.cpp
@@ -49,11 +49,11 @@ composition Main
 
         BOOST_CHECK_EQUAL(phrase.Name, "Foo");
         BOOST_CHECK(phrase.Attributes.empty());
-        BOOST_CHECK_EQUAL(phrase.Location.Line, 3);
-        BOOST_CHECK_EQUAL(phrase.Location.Column, 0);
+        BOOST_CHECK_EQUAL(phrase.Location.Line, 3u);
+        BOOST_CHECK_EQUAL(phrase.Location.Column, 0u);
 
-        BOOST_CHECK_EQUAL(phrase.Block.Location.Line, 4);
-        BOOST_CHECK_EQUAL(phrase.Block.Location.Column, 0);
+        BOOST_CHECK_EQUAL(phrase.Block.Location.Line, 4u);
+        BOOST_CHECK_EQUAL(phrase.Block.Location.Column, 0u);
 
         {
             auto noteSeqStatement = boost::get<YAMML::AST::NoteSequenceStatement>(phrase.Block.Sequences.at(0));
@@ -62,10 +62,10 @@ composition Main
             BOOST_REQUIRE(noteSeqStatement.NoteSeq.is_initialized());
             auto noteSeq = *noteSeqStatement.NoteSeq;
 
-            BOOST_CHECK_EQUAL(noteSeq.Notes.size(), 1);
+            BOOST_CHECK_EQUAL(noteSeq.Notes.size(), 1u);
 
             auto noteAndExpr = noteSeq.Notes.at(0);
-            BOOST_CHECK_EQUAL(noteAndExpr.Notes.size(), 3);
+            BOOST_CHECK_EQUAL(noteAndExpr.Notes.size(), 3u);
 
             {
                 auto noteAndDuration = boost::get<AST::NoteAndDuration>(noteAndExpr.Notes.at(0));
@@ -98,20 +98,20 @@ composition Main
 
         BOOST_CHECK_EQUAL(composition.Name, "Main");
         BOOST_CHECK(composition.Attributes.empty());
-        BOOST_CHECK_EQUAL(composition.Location.Line, 8);
-        BOOST_CHECK_EQUAL(composition.Location.Column, 0);
+        BOOST_CHECK_EQUAL(composition.Location.Line, 8u);
+        BOOST_CHECK_EQUAL(composition.Location.Column, 0u);
 
-        BOOST_CHECK_EQUAL(composition.Statements.size(), 1);
+        BOOST_CHECK_EQUAL(composition.Statements.size(), 1u);
 
         auto trackListBlock = boost::get<AST::TrackListBlock>(composition.Statements.at(0));
-        BOOST_CHECK_EQUAL(trackListBlock.Tracks.size(), 1);
+        BOOST_CHECK_EQUAL(trackListBlock.Tracks.size(), 1u);
 
         auto trackBlock = trackListBlock.Tracks.at(0);
         BOOST_CHECK_EQUAL(trackBlock.TrackNumber, 1);
         BOOST_CHECK(trackBlock.Attributes.empty());
-        BOOST_CHECK_EQUAL(trackBlock.Location.Line, 12);
-        BOOST_CHECK_EQUAL(trackBlock.Location.Column, 8);
-        BOOST_CHECK_EQUAL(trackBlock.Items.size(), 1);
+        BOOST_CHECK_EQUAL(trackBlock.Location.Line, 12u);
+        BOOST_CHECK_EQUAL(trackBlock.Location.Column, 8u);
+        BOOST_CHECK_EQUAL(trackBlock.Items.size(), 1u);
 
         auto trackItem = trackBlock.Items.at(0);
 
@@ -165,15 +165,15 @@ composition Main
     BOOST_REQUIRE(itComposition != module->Compositions.end());
 
     BOOST_CHECK_EQUAL(itComposition->Name, "Main");
-    BOOST_CHECK_EQUAL(itComposition->Attributes.size(), 1);
+    BOOST_CHECK_EQUAL(itComposition->Attributes.size(), 1u);
     
     auto attr = itComposition->Attributes.at(0);
 
     BOOST_CHECK_EQUAL(attr.Name, "foo");
-    BOOST_CHECK_EQUAL(attr.Location.Line, 3);
-    BOOST_CHECK_EQUAL(attr.Location.Column, 0);
+    BOOST_CHECK_EQUAL(attr.Location.Line, 3u);
+    BOOST_CHECK_EQUAL(attr.Location.Column, 0u);
 
-    BOOST_CHECK_EQUAL(attr.Arguments.size(), 1);
+    BOOST_CHECK_EQUAL(attr.Arguments.size(), 1u);
 
     auto arg = attr.Arguments.at(0);
 
@@ -184,11 +184,11 @@ composition Main
 
     BOOST_CHECK_EQUAL(boost::get<std::string>(argValue), u8"abc\u2661");
 
-    BOOST_CHECK_EQUAL(arg.Location.Line, 3);
-    BOOST_CHECK_EQUAL(arg.Location.Column, 5);
+    BOOST_CHECK_EQUAL(arg.Location.Line, 3u);
+    BOOST_CHECK_EQUAL(arg.Location.Column, 5u);
 
-    BOOST_CHECK_EQUAL(arg.Value.Location.Line, 3);
-    BOOST_CHECK_EQUAL(arg.Value.Location.Column, 9);
+    BOOST_CHECK_EQUAL(arg.Value.Location.Line, 3u);
+    BOOST_CHECK_EQUAL(arg.Value.Location.Column, 9u);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
