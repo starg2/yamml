6a9dfe40083fc5aff29018fd289d25adf58ce5ed
starg <starg@users.osdn.me>
2016-08-20 21:56:37 +0900

    エラーメッセージを追加

--- src/driver/msgcallback.cpp
+++ src/driver/msgcallback.cpp
@@ -13,7 +13,38 @@ namespace YAMML
 namespace Driver
 {
 
-MessagePrinter::MessagePrinter(IStdErrWriter* pStdErrWriter) : m_pStdErrWriter{pStdErrWriter}
+namespace
+{
+
+const std::string ICEMessage =
+    "An internal compiler error has occurred during the compilation process. "
+    "Please submit a bug report with full source code if appropriate.";
+
+}
+
+MessagePrinter::MessagePrinter(IStdErrWriter* pStdErrWriter)
+    : m_KindTextMap{
+        {Message::MessageKind::FetalError, "fetal error"},
+        {Message::MessageKind::Error, "error"},
+        {Message::MessageKind::Warning, "warning"},
+        {Message::MessageKind::Note, "note"}
+    },
+    m_IDTextMap{
+        {Message::MessageID::Unknown, ICEMessage},
+        {Message::MessageID::UnknownInPhrase2IR, ICEMessage + " (Phrase2IR: Phrase = '{0}', Message = '{1}')"},
+        {Message::MessageID::UnknownInComposition2IR, ICEMessage + " (Composition2IR: Composition = '{0}', Message = '{1}')"},
+        {Message::MessageID::UnknownInIR2MIDI, ICEMessage + " (IR2MIDI: Message = '{0}')"},
+        {Message::MessageID::UnprocessedCommand, ICEMessage + " (Unprocessed command in '{0}')"},
+        {Message::MessageID::UnprocessedAttribute, ICEMessage + " (Unprocessed attribute in '{0}')"},
+        {Message::MessageID::DuplicatedCompositionName, "composition named '{0}' is already defined."},
+        {Message::MessageID::DuplicatedPhraseName, "phrase named '{0}' is already defined."},
+        {Message::MessageID::NoSuchPhraseName, "cannot find a phrase named '{0}'."},
+        {Message::MessageID::NoSuchCompositionName, "cannot find a composition named '{0}'."},
+        {Message::MessageID::TrackNumberIsOutOfSafeRange, "track number '{0}' is out of range. (must be 0 <= # < {1})"},
+        {Message::MessageID::TrackNumberIsOutOfPreferredRange, "track number '{0}' is out of range. (must be 0 <= # < {1})"},
+        {Message::MessageID::TooLargeRepeatCount, "repeat count '{0}' is too large. (must be <= {1})"}
+    },
+    m_pStdErrWriter{pStdErrWriter}
 {
     if (!pStdErrWriter)
     {
